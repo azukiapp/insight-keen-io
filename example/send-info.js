21a24d3216eab2bac8b8f75856f9401124d72c49
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-10 16:27:45 -0300

    README updated

--- example/send-info.js
+++ example/send-info.js
@@ -1,21 +1,21 @@
 'use strict';
-var InsightKeenIo = require('../lib');
+var Insight = require('../lib');
 
-var InsightKeenIo = new InsightKeenIo({
+var insight = new Insight({
 	projectId: '5526968d672e6c5a0d0ebec6',
 	writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768'
 });
 
 // FIXME: must be stored
-// InsightKeenIo.askPermission('Do you accept?', function () {
-// 	var tracking_allowed = !(InsightKeenIo.optOut);
+// insight.askPermission('Do you accept?', function () {
+// 	var tracking_allowed = !(insight.optOut);
 // 	trackIt();
 // });
 
 trackIt();
 
 function trackIt() {
-	var result = InsightKeenIo.track('SUBJECT', {
+	var result = insight.track('SUBJECT', {
 		event_type: 'pull',
 		state: 'ok',
 		reason: '',
