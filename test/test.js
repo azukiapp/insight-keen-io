ec1b3e14f7e58c5cb1e3130ee9f749b748b3d45e
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-11 17:03:59 -0300

    throw errors even on forks

--- test/test.js
+++ test/test.js
@@ -7,48 +7,8 @@ var objectValues = require('object-values');
 var InsightKeenIo = require('../lib');
 
 describe('InsightKeenIo()', function () {
-	var insight = new InsightKeenIo({
-		projectId: 'YOUR_PROJECT_ID',
-		writeKey: 'YOUR_WRITE_KEY'
-	});
-
-	// it('should put tracked path in queue', function (cb) {
-	// 	var dataGiven = {
-	// 		"event_type" : "start",
-	// 		"sessionId"  : "azk_agent_session_id_3890217389127389",
-	// 		"ip_address" : "${keen.ip}",
-	// 	};
-	//
-	// 	InsightKeenIo.prototype._save = function() {
-	// 		assert.equal(dataGiven, objectValues(this._queue)[0]);
-	// 		cb();
-	// 	};
-	//
-	// 	insight.track("agent", dataGiven);
-	// });
-
 	it('should track work without fork', function (done) {
-
-		var insight = new InsightKeenIo({
-			projectId: '5526968d672e6c5a0d0ebec6',
-			writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
-			use_fork: false
-		});
-
-		var dataGiven = {
-			"sessionId"  : "azk_agent_session_id_3890217389127389",
-			"ip_address" : "${keen.ip}",
-		};
-
-		insight.track("FROM_TEST", dataGiven, function (data) {
-			assert.ok(data[0].created);
-			done();
-		});
-
-	});
-
-	it('should track work without fork', function (done) {
-
+		this.timeout(10000);
 		var insight = new InsightKeenIo({
 			projectId: '5526968d672e6c5a0d0ebec6',
 			writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
@@ -67,7 +27,7 @@ describe('InsightKeenIo()', function () {
 	});
 
 	it('should track work with fork', function (done) {
-
+		this.timeout(10000);
 		var insight = new InsightKeenIo({
 			projectId: '5526968d672e6c5a0d0ebec6',
 			writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
@@ -78,8 +38,8 @@ describe('InsightKeenIo()', function () {
 			"ip_address" : "${keen.ip}",
 		};
 
-		insight.track("FROM_TEST_WITH_FORK", dataGiven, function (is_ok) {
-			assert.ok(is_ok);
+		insight.track("FROM_TEST_WITH_FORK", dataGiven, function (is_zero) {
+			assert.equal(is_zero, 0);
 			done();
 		});
 
