bf55cf472cd9ce42fa31b2b3e1b68b0716e356e5
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-11 16:30:21 -0300

    request-push

--- example/send-info.js
+++ example/send-info.js
@@ -1,20 +1,6 @@
 'use strict';
 var InsightKeenIo = require('../lib');
 
-var insight = new InsightKeenIo({
-	projectId: '5526968d672e6c5a0d0ebec6',
-	writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
-	fork_silent: false
-});
-
-// FIXME: must be stored
-insight.askPermission('Do you accept?', function () {
-	var tracking_allowed = !(insight.optOut);
-	if (tracking_allowed) {
-		trackIt();
-	}
-});
-
 function trackIt() {
 	var result = insight.track('SUBJECT', {
 		event_type: 'pull',
@@ -39,3 +25,19 @@ function trackIt() {
     "ip_address" : "${keen.ip}"
 	});
 }
+
+var insight = new InsightKeenIo({
+	projectId: '5526968d672e6c5a0d0ebec6',
+	writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
+	fork_silent: false
+});
+
+trackIt();
+
+// FIXME: must be stored
+// insight.askPermission('Do you accept?', function () {
+// 	var tracking_allowed = !(insight.optOut);
+// 	if (tracking_allowed) {
+// 		trackIt();
+// 	}
+// });
--- test/test.js
+++ test/test.js
@@ -12,20 +12,60 @@ describe('InsightKeenIo()', function () {
 		writeKey: 'YOUR_WRITE_KEY'
 	});
 
-	it('should put tracked path in queue', function (cb) {
+	// it('should put tracked path in queue', function (cb) {
+	// 	var dataGiven = {
+	// 		"event_type" : "start",
+	// 		"sessionId"  : "azk_agent_session_id_3890217389127389",
+	// 		"ip_address" : "${keen.ip}",
+	// 	};
+	//
+	// 	InsightKeenIo.prototype._save = function() {
+	// 		assert.equal(dataGiven, objectValues(this._queue)[0]);
+	// 		cb();
+	// 	};
+	//
+	// 	insight.track("agent", dataGiven);
+	// });
+
+	it('should track work without fork', function (done) {
+
+		var insight = new InsightKeenIo({
+			projectId: '5526968d672e6c5a0d0ebec6',
+			writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
+			run_fork: false
+		});
+
 		var dataGiven = {
-			"event_type" : "start",
 			"sessionId"  : "azk_agent_session_id_3890217389127389",
 			"ip_address" : "${keen.ip}",
 		};
 
-		InsightKeenIo.prototype._save = function() {
-			assert.equal(dataGiven, objectValues(this._queue)[0]);
-			cb();
-		};
+		insight.track("FROM_TEST", dataGiven, function (data) {
+			assert.ok(data[0].created);
+			done();
+		});
 
-		insight.track("agent", dataGiven);
 	});
+
+	it('should track work without fork', function (done) {
+
+		var insight = new InsightKeenIo({
+			projectId: '5526968d672e6c5a0d0ebec6',
+			writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
+			run_fork: false
+		});
+
+		var dataGiven = {
+			"ip_address" : "${keen.ip}",
+		};
+		
+		insight.track("FROM_TEST", dataGiven, function (data) {
+			assert.ok(data[0].created);
+			done();
+		});
+
+	});
+
 
 	it('should throw exception when trackingCode or packageName is not provided', function (cb) {
 		assert.throws(function () {
