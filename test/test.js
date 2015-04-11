97248c5e768d5788de8aacd48f2d97684b269e35
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-11 16:38:35 -0300

    request-push refactor

--- test/test.js
+++ test/test.js
@@ -32,7 +32,7 @@ describe('InsightKeenIo()', function () {
 		var insight = new InsightKeenIo({
 			projectId: '5526968d672e6c5a0d0ebec6',
 			writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
-			run_fork: false
+			use_fork: false
 		});
 
 		var dataGiven = {
@@ -52,13 +52,13 @@ describe('InsightKeenIo()', function () {
 		var insight = new InsightKeenIo({
 			projectId: '5526968d672e6c5a0d0ebec6',
 			writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
-			run_fork: false
+			use_fork: false
 		});
 
 		var dataGiven = {
 			"ip_address" : "${keen.ip}",
 		};
-		
+
 		insight.track("FROM_TEST", dataGiven, function (data) {
 			assert.ok(data[0].created);
 			done();
