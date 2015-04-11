81101b1dcd3424a9a6e8813ccf63ffe235c84220
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-11 16:44:52 -0300

    request-push refactor

--- test/test.js
+++ test/test.js
@@ -66,6 +66,25 @@ describe('InsightKeenIo()', function () {
 
 	});
 
+	it('should track work with fork', function (done) {
+
+		var insight = new InsightKeenIo({
+			projectId: '5526968d672e6c5a0d0ebec6',
+			writeKey: '5dbce13e376070e36eec0c7dd1e7f42e49f39b4db041f208054617863832309c14a797409e12d976630c3a4b479004f26b362506e82a46dd54df0c977a7378da280c05ae733c97abb445f58abb56ae15f561ac9ad774cea12c3ad8628d896c39f6e702f6b035541fc1a562997cb05768',
+			use_fork: true
+		});
+
+		var dataGiven = {
+			"ip_address" : "${keen.ip}",
+		};
+
+		insight.track("FROM_TEST_WITH_FORK", dataGiven, function (is_ok) {
+			assert.ok(is_ok);
+			done();
+		});
+
+	});
+
 
 	it('should throw exception when trackingCode or packageName is not provided', function (cb) {
 		assert.throws(function () {
